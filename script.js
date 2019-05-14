28ed9847598ad170f3b1aace4a5627910792e0cb
Lyudamila1992 <39173022+Lyudamila1992@users.noreply.github.com>
2019-05-15 00:33:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -168,35 +168,27 @@ setTimeout(function run() {
   Grass.prototype= {
       
     
-    alto_hierba: 220,    // grass height
-    maxAngle:    0,    // maximum grass rotation angle (wind movement)
-    angle:       0,    // construction angle. thus, every grass is different to others  
-    coords:      null,  // quadric bezier curves coordinates
-    color:       null,  // grass color. modified by ambient component.
-    offset_control_point:   3,    // grass base width. greater values, wider at the basement.
+    alto_hierba: 220,    
+    maxAngle:    0,    
+    angle:       0,    
+    coords:      null,  
+    color:       null,  
+    offset_control_point:   3,    
 
     initialize : function(canvasWidth, canvasHeight, minHeight, maxHeight, angleMax, initialMaxAngle)  {
 
-      // grass start position
+
       var sx= Math.floor( Math.random()*canvasWidth );
       var sy = canvasHeight;
-      
-      // quadric curve middle control point. higher values means wider grass from base to peak.
-      // try offset_control_x=10 for thicker grass.
       var offset_control_x=1.5;  
       
-      //this.alto_hierba= minHeight+Math.random()*maxHeight;
       this.maxAngle= 10+Math.random()*angleMax;
       this.angle= Math.random()*initialMaxAngle*(Math.random()<0.5?1:-1)*Math.PI/180;
 
-      // hand crafted value. modify offset_control_x to play with grass curvature slope.
       var csx= sx-offset_control_x ;
 
-      // grass curvature. greater values make grass bender. 
-      // try with:  
-    var csy= sy-this.alto_hierba;  //-> much more bended grass.
-      //        var csy= sy-1;                 -> totally unbended grass.
-  var csy= sy-this.alto_hierba/2; //-> original. good looking grass.
+    var csy= sy-this.alto_hierba;  
+    var csy= sy-this.alto_hierba/2; 
       var csy= 0;
       if ( Math.random()<0.1 ) {
         csy= sy-this.alto_hierba;
@@ -204,47 +196,27 @@ setTimeout(function run() {
         csy= sy-this.alto_hierba/2;
       }
           
-      /**
-       I determined that both bezier curves that conform each grass should have
-       the same middle control point to be parallel.
-       You can play with psx/psy adding or removing values to slightly modify grass
-       geometry.
-      **/
       var psx= csx;
-      // changed var psy= csy; to
       var psy= csy-offset_control_x;
           
-      // the bigger offset_control_point, the wider on its basement.
       this.offset_control_point=5;
       var dx= sx+this.offset_control_point;
       var dy= sy;      
       
       this.coords= [sx,sy,csx,csy,psx,psy,dx,dy];
           
-      // grass color.
       this.color= [16+Math.floor(Math.random()*32),
                    100+Math.floor(Math.random()*155),
                    16+Math.floor(Math.random()*32) ];
       
     },
-    
-    /**
-     * paint every grass.
-     * @param ctx is the canvas2drendering context
-     * @param time for grass animation.
-     * @param ambient parameter to dim or brighten every grass.
-     * @returns nothing
-     */
+
     paint : function(ctx,time,ambient) {
 
           ctx.save();
-          
-          // grass peak position. how much to rotate the peak.
-          // less values (ie the .0005), will make as if there were a softer wind.
+        
           var inc_punta_hierba= Math.sin(time*0.0005);
           
-          // rotate the point, so grass curves are modified accordingly. If just moved horizontally, the curbe would
-          // end by being unstable with undesired visuals. 
           var ang= this.angle + Math.PI/2 + inc_punta_hierba * Math.PI/180*(this.maxAngle*Math.cos(time*0.0002));
           var px= this.coords[0]+ this.offset_control_point + this.alto_hierba*Math.cos(ang);
           var py= this.coords[1]                  - this.alto_hierba*Math.sin(ang);
@@ -293,10 +265,10 @@ setTimeout(function run() {
         g.initialize(
             width,
             height,
-            50,      // min grass height 
-            height*2/3, // max grass height
-            20,     // grass max initial random angle 
-            40      // max random angle for animation 
+            50,      
+            height*2/3, 
+            20,     
+            40      
             );
         this.grass.push(g);
       }
@@ -311,32 +283,24 @@ setTimeout(function run() {
     paint : function(ctx, time){
       ctx.save();
       
-      // draw stars if ambient below .3 -> night
       if ( this.ambient<0.3 )  {
         
-        // modify stars translucency by ambient (as transitioning to day, make them dissapear).
         ctx.globalAlpha= 1-((this.ambient-0.05)/0.25);
 
-        // as well as making them dimmer
         intensity= 1 - (this.ambient/2-0.05)/0.25;
-        
-        // how white do you want the stars to be ??
+
         var c= Math.floor( 192*intensity );
         var strc= 'rgb('+c+','+c+','+c+')';
         ctx.strokeStyle=strc;
         
-        // first num_fireflyes coordinates are fireflyes themshelves.
         for( var j=this.num_fireflyes*2; j<this.stars.length; j+=2 )  {
           var inc=1;
           
-          // every one out of 3 stars move at 1.5 increment
           if ( j%3===0 ) {
             inc=1.5;
           } else if ( j%11===0 ) {
-          // every one out of 11 stars move at 2.5 increment
             inc=2.5;
           }
-          // all the others at increment 1
           this.stars[j]= (this.stars[j]+0.1*inc)%canvas.width;
           
           var y= this.stars[j+1];
@@ -348,7 +312,6 @@ setTimeout(function run() {
       ctx.globalAlpha= 1;
       
       var i;
-      // draw fireflyes
         ctx.fillStyle= '#ffff00';      
         for(i=0; i<this.num_fireflyes*2; i+=2) {
           var angle= Math.PI*2*Math.sin(time*3E-4) + i*Math.PI/50;
@@ -356,12 +319,12 @@ setTimeout(function run() {
           ctx.fillRect( 
               this.width/2 + 
               0.5*this.stars[i] + 
-              150*Math.cos(time*3E-4) * Math.sin(time*0.00001*i) +  // move horizontally with time 
+              150*Math.cos(time*3E-4) * Math.sin(time*0.00001*i) +  
               radius*Math.cos(angle),
               
               this.height/2 + 
               0.5*this.stars[i+1] +  
-              20*Math.sin(time*3E-4) * 5* Math.cos(time*0.00001*i)+  // move vertically with time 
+              20*Math.sin(time*3E-4) * 5* Math.cos(time*0.00001*i)+  
               radius*Math.sin(angle),
               
                 3,
@@ -396,11 +359,6 @@ setTimeout(function run() {
       
     }
     
-    /**
-     * fade sky colors
-     * @param time current time
-     * @param last how much time to take fading colors
-     */
     function lerp( time, last ) {
       gradient= ctx.createLinearGradient(0,0,0,canvas.height);
       
@@ -421,8 +379,8 @@ setTimeout(function run() {
     }
 
     
-var lerpTime= 15000;    // time taken to fade sky colors
-var nextLerpTime= 15000;  // after fading, how much time to wait to fade colors again.
+var lerpTime= 15000;    
+var nextLerpTime= 15000;  
 
 var interval= null;
 var canvas= null;
@@ -470,10 +428,8 @@ var time;
               0xa0, 0x1f, 0x1f,
               0xff, 0x7f, 0x00 ] ];
     
-    // ambient intensities for each sky color
     ambients= [ 1, 0.35, 0.05, 0.5 ];
     
-    // start with this sky index.
     lerpindex= 0;
     
 window.addEventListener(
